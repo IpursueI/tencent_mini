9ff2b24026a63c66d8849c0ad0b8bfe08157bd26
wusanye <1419816717@qq.com>
2016-06-26 10:37:02 +0800

    更改models里的第三张表的字段名称

--- mini/pet/models.py
+++ mini/pet/models.py
@@ -34,9 +34,9 @@ class Activity(models.Model):
         return self.activity_introduction
 
 class Participant(models.Model):
-    participant_user_id = models.ForeignKey(User)
-    participant_activity_id = models.ForeignKey(Activity)
+    participant_user = models.ForeignKey(User)
+    participant_activity = models.ForeignKey(Activity)
     participant_user_type = models.IntegerField(default = 0)
 
     def __unicode__(self):
-        return self.participant_activity_id
+        return self.participant_activity
