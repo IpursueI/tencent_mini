f152dc79906bf37fabad980823043e33c04efc80
perryhuang <pengjianghuang@163.com>
2016-06-26 23:30:35 +0800

    添加发布收养信息，添加获取详细信息

---
+++ mini/pet/getAdoptDetailList.py
@@ -0,0 +1,59 @@
+#-*- coding:UTF-8 -*-
+from django.http import HttpResponse
+import models
+import datetime
+import util
+import json
+import hashlib
+
+'''
+获取详细收养信息, by stanwu 2016/06/26
+post参数名：method
+post参数内容：{"name":"getAdoptDetailList","args":{"activity_id":xxx}}
+
+服务器返回值(json格式)：
+    请求失败：
+    {"retValue": "", "retCode": -1, "retMsg": "xxxxx"}
+    请求成功：
+    {"retValue":[{"user_id","xxx", "activity_picture":xxx, "user_avatar":xxx, "user_nickname":"xxx",
+	"user_age":xxx, "user_interest":xxx, "activity_pet_type":xxx, "activity_price":xxx,
+	"activity_address":"xxx", "activity_introduction":"xxx","activity_start_time":"xxx",
+	"activity_end_time":"xxx"}],
+ 	"retCode": 0,
+ 	"retMsg": ""}
+'''
+
+def getAdoptDetailList(data):
+
+    activityId = data.get("activity_id")
+    if not activityId:
+        return util.errorJsonWrapper("收养详细信息获取请求错误,不存在该activity_id")
+
+    activity = models.Activity.objects.get(pk = activityId)
+    if not activity:
+        return util.errorJsonWrapper("收养详细信息获取请求错误,不存在该活动")
+
+    user = models.Participant.objects.filter(participant_activity = activity).first().participant_user
+    if not user:
+        return util.errorJsonWrapper("收养详细信息获取请求错误,不存在该用户")
+
+    # check token by stanwu
+    token = data.get("user_token")
+    if token != user.user_token:
+        return util.errorJsonWrapper("token错误")
+
+    detailDict = {  "user_id" : user.user_id, "activity_picture" : activity.activity_picture, "user_avatar" : user.user_avatar,
+                    "user_nickname" : user.user_nickname, "user_age" : user.user_age, "user_interest" : user.user_interest,
+                    "activity_pet_type" : activity.activity_pet_type, "activity_price" : activity.activity_price,
+                    "activity_address": activity.activity_address, "activity_introduction": activity.activity_introduction,
+                    "activity_start_time": activity.activity_start_time,
+                    "activity_end_time": activity.activity_end_time
+                    }
+
+    retList = []
+    retList.append(detailDict)
+    res = dict(retCode = 0, retMsg = "", retValue = retList)
+
+    return json.dumps(res)
+
+
---
+++ mini/pet/issueAdoptPetInfo.py
@@ -0,0 +1,70 @@
+#-*- coding:UTF-8 -*-
+from django.http import HttpResponse
+import models
+import datetime
+import util
+import json
+import hashlib
+
+'''
+发布收养信息：by stanwu, 2016/06/25
+post参数名：method
+post参数内容：
+	{"name":"issueAdoptPetInfo","args":{"user_id":"xxx","activity_picture":xxx, "activity_introduction":"xxx",
+ 	"activity_address":"xxx","activity_longitude":"xxx","activity_latitude":"xxx",
+	"activity_pet_type":xxx,"activity_price":xxx,
+	"activity_start_time":"xxx", "activity_end_time":"xxx"}}
+
+服务器返回值(json格式):
+    注册成功：
+    {"retValue": "", "retCode": 0, "retMsg": ""}
+    注册失败：
+    {"retValue": "", "retCode": -1, "retMsg": "xxxxx"}
+'''
+
+def issueAdoptPetInfo(data):
+    EVENT_STATUS = 0
+    ADOPT = 1
+    userId = data.get("user_id")
+    user = models.User.objects.filter(user_id = userId).first()
+    if not user:
+        return util.errorJsonWrapper("不存在该用户")
+
+    # check token by stanwu
+    token = data.get("user_token")
+    if token != user.user_token:
+        return util.errorJsonWrapper("token错误")
+
+    activityPic = data.get("activity_picture")
+    activityIntro = data.get("activity_introduction")
+    activityAddr = data.get("activity_address")
+    activityLongi = data.get("activity_longitude")
+    activityLati = data.get("activity_latitude")
+    activityPet = data.get("activity_pet_type")
+    activityPrice = data.get("activity_price")
+    activityStartTime = data.get("activity_start_time")
+    activityEndTime = data.get("activity_end_time")
+
+    try:
+        activity = models.Activity( activity_introduction = activityIntro,
+                                activity_picture = activityPic,
+                                activity_price = activityPrice,
+                                activity_pet_type = activityPet,
+                                activity_start_time = activityStartTime,
+                                activity_end_time = activityEndTime,
+                                activity_status = EVENT_STATUS,
+                                activity_address = activityAddr)
+        activity.save()
+    except Exception:
+        return util.errorJsonWrapper("发布收养信息出错，activity无法写入数据库")
+
+    try:
+        participant = models.Participant(participant_user = user,
+                                     participant_activity = activity,
+                                     participant_user_type = ADOPT)
+        participant.save()
+    except Exception:
+        return util.errorJsonWrapper("发布收养信息出错，participant无法写入数据库")
+
+    return util.simpleOkJsonWrapper()
+
